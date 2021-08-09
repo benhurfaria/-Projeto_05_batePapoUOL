7004fc44adadf07692f0d652ab4a309b3af7cfaf
Ben Hur <benhurfaria3@gmail.com>
2021-08-09 11:48:25 -0300

    Colocando funcionalidade de carregar mensagens, scroll e tratamento de erro

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const URL_CHAT = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
 const URL_CONVERSA = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages";
+const URL_MANTER = "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status";
 let nome;
 let mensagem;
 let mensagens = [];
+let lastOne;
+let primeiraVez = 0;
+let ultimoAnterior = "";
+let proximoBool = false;
 //comecando a cuidar dos participantes
 
 function inputNome() {
@@ -16,12 +21,19 @@ function inputNome() {
 inputNome();
 function colocarNome(nameEntra) {
     const promessa = axios.post(URL_CHAT, nameEntra);
-    promessa.then(pegarNomes);
+    promessa.then(pegarNomes);   
+    promessa.catch(voltaPedirNome);
+}
+function voltaPedirNome(error){
+    alert("Escreva outro nome");
+    inputNome();
 }
 
 function pegarNomes(resposta) {
     const promessa = axios.get(URL_CHAT);
+    console.log("mel");
     promessa.then(console.log);
+    inputMensagem();
 }
 
 //terminando a cuidar dos participantes
@@ -30,22 +42,26 @@ function pegarNomes(resposta) {
 function inputMensagem() {
     mensagem = document.querySelector("input").value;
     document.querySelector("input").value = "";
-    console.log(mensagem);
-    let mensagemEntre = {
-        from: nome,
-        to: "Todos",
-        text: mensagem, //mensagem
-        type: "message",
-        time: "08:02:50"
-    };
-
-    colocarMensagem(mensagemEntre);
+    if (mensagem.length > 0) {
+        console.log(mensagem);
+        let mensagemEntre = {
+            from: nome,
+            to: "Todos",
+            text: mensagem,
+            type: "message",
+            time: ""
+        };
+        colocarMensagem(mensagemEntre);
+    } else {
+        pegarMensagem();
+    }
 }
 
 function colocarMensagem(mensagemEntre) {
     const promessa = axios.post(URL_CONVERSA, mensagemEntre);
     promessa.then(pegarMensagem);
 }
+
 
 function pegarMensagem() {
     const promessa = axios.get(URL_CONVERSA);
@@ -54,7 +70,7 @@ function pegarMensagem() {
 
 function carregarMensagem(resposta) {
     mensagens = resposta.data;
-    console.log(mensagens);
+    //console.log(mensagens);
     renderizarMensagens();
 }
 //terminando cuidar das mensagens
@@ -62,11 +78,112 @@ function carregarMensagem(resposta) {
 function renderizarMensagens() {
     let divConversas = document.querySelector(".conversa");
     divConversas.innerHTML = "";
-    for (let i = 0; i < mensagens.length; i++) {
-        if (mensagens[i].type === "status") {
-            divConversas.innerHTML += `<div class="entrou">
-                <p>(${mensagens[i].time}) <strong>${mensagens[i].from}</strong> ${mensagens[i].text} </p>
-            </div>`;
-        }
+    let ultimoBool = false;
+
+    if (ultimoAnterior === mensagens[mensagens.length - 1].time && proximoBool === false) {
+        console.log(ultimoAnterior);
+        console.log(mensagens[mensagens.length - 1].time);
+        ultimoBool = true;
+        proximoBool = true;
     }
+    let nessaRodada = ultimoAnterior;
+    for (let i = 0; i < mensagens.length; i++) {
+
+        if (mensagens[i].type === "status") {
+            if (i === mensagens.length - 1) {
+
+                lastOne = `<div class="entrou ultimo">
+                <p>(${mensagens[i].time}) <strong>${mensagens[i].from}</strong> ${mensagens[i].text} </p>
+                </div>`;
+                
+                divConversas.innerHTML += lastOne;
+                ultimoAnterior = mensagens[i].time;
+            } else {
+
+                divConversas.innerHTML += `<div class="entrou">
+            <p>(${mensagens[i].time}) <strong>${mensagens[i].from}</strong> ${mensagens[i].text} </p>
+            </div>`;
+
+            }
+        }
+
+        if (mensagens[i].type === "message") {
+            if (i === mensagens.length - 1) {
+
+                lastOne = `<div class="mensagem-comum ultimo">
+                <p>(${mensagens[i].time})  <strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}</strong>:  ${mensagens[i].text}</p>
+                </div>`;
+
+                divConversas.innerHTML += lastOne;
+
+                ultimoAnterior = mensagens[i].time;
+            } else {
+
+                divConversas.innerHTML += `<div class="mensagem-comum">
+            <p>(${mensagens[i].time})  <strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}</strong>:  ${mensagens[i].text}</p>
+        </div>`;
+
+            }
+
+
+        }
+
+        if (mensagens[i].type === "private_message" && mensagens[i].to === nome) {
+            if (i === mensagens.length - 1) {
+
+                lastOne = `<div class="reservadamente ultimo">
+                <p>(${mensagens[i].time})  <strong>${mensagens[i].from}</strong> reservadamente para <strong>${mensagens[i].to}</strong>:  ${mensagens[i].text}</p>
+            </div>`;
+
+                divConversas.innerHTML += lastOne
+                ultimoAnterior = mensagens[i].time;
+
+            } else {
+
+                divConversas.innerHTML += `<div class="reservadamente">
+            <p>(${mensagens[i].time})  <strong>${mensagens[i].from}</strong> reservadamente para <strong>${mensagens[i].to}</strong>:  ${mensagens[i].text}</p>
+        </div>`;
+
+            }
+        }
+
+
+    }
+    //faz aki
+    if(ultimoAnterior === nessaRodada){
+        proximoBool = true;
+    }else{
+        proximoBool = false;
+    }
+
+    if(ultimoBool && proximoBool){
+        let divUltimo = document.querySelector(".ultimo");
+        console.log("pq???")
+        divUltimo.scrollIntoView();
+    }
+    if(primeiraVez === 0){
+        let divUltimo1 = document.querySelector(".ultimo");
+        divUltimo1.scrollIntoView();
+    }
+    
+    primeiraVez++;
+
 }
+
+setInterval(pegarMensagem, 3000);
+
+//Manter conexao
+function inputManterConexao() {
+    let nameEntra = {
+        name: nome
+    };
+    manterConexao(nameEntra);
+}
+
+function manterConexao(nameEntra) {
+    const promessa = axios.post(URL_MANTER, nameEntra);
+    promessa.then();
+}
+
+setInterval(inputManterConexao, 5000);
+//Fim de manter Conexao
